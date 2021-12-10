128b0f4d94699025e06b200e0fb477d8c6899dce
Blexie <fraser.blexie@gmail.com>
2021-12-10 19:57:21 +0000

    New branch...

---
+++ app/main.py
@@ -0,0 +1,110 @@
+import tweepy
+import praw
+import hottakesauth
+from random import randrange, choice
+from time import sleep
+
+# Twitter Setup
+CONSUMER_KEY = hottakesauth.CONSUMER_KEY
+CONSUMER_SECRET = hottakesauth.CONSUMER_SECRET
+ACCESS_TOKEN = hottakesauth.ACCESS_TOKEN
+ACCESS_TOKEN_SECRET = hottakesauth.ACCESS_TOKEN_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY,
+                           CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN,
+                      ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+# Reddit Auth
+reddit = praw.Reddit(
+    client_id=hottakesauth.CLIENT_ID,
+    client_secret=hottakesauth.CLIENT_SECRET,
+    user_agent=hottakesauth.USER_AGENT,
+)
+
+
+# Generate tweet body (controversial threads)
+def generatetweet():
+    for submission in reddit.subreddit("formula1").controversial("day", limit=1):
+        submission.comment_sort = "controversial"
+        if not submission.stickied or "Race Discussion" in submission.title:
+          for comment in submission.comments:
+            if comment.author == "automoderator" or comment.stickied or comment.body == "[removed]" or len(comment.body) > 280:
+                continue
+            else:
+                COMMENT = comment.body
+                ID = comment.id
+                break
+    return COMMENT, ID
+
+
+# Generate tweet body (hot threads)
+def generatetweethot():
+    for submission in reddit.subreddit("formula1").hot(limit=10):
+        submission.comment_sort = "controversial"
+        if not submission.stickied or "Race Discussion" in submission.title:
+          for comment in submission.comments:
+            if comment.author == "automoderator" or comment.stickied or comment.body == "[removed]" or len(comment.body) > 280:
+                continue
+            else:
+                COMMENT = comment.body
+                ID = comment.id
+                break
+    return COMMENT, ID
+
+
+# Random @ Mention
+def randomat():
+    with open("ats.txt") as file:
+        ATS = file.readlines()
+        ATS = [line.rstrip() for line in ATS]
+
+    NUM_ATS = len(ATS)
+    return ATS[randrange(NUM_ATS)]
+
+
+def sendit():
+    # Choose hot or controversial thread
+    hotornot = [generatetweet, generatetweethot]
+    COMMENT = choice(hotornot)()
+
+    RBRMATCHES = ["rbr", "RBR", "Max", "Red Bull", "Jos"]
+    WILLIAMSMATCHES = ["williams"]
+    RUSSELLMATCHES = ["Russell"]
+
+    if any(x in COMMENT[0] for x in RBRMATCHES):
+        AT = "@redbullracing"
+    elif any(x in COMMENT[0] for x in WILLIAMSMATCHES):
+        AT = "@WilliamsRacing"
+    elif any(x in COMMENT[0] for x in RUSSELLMATCHES):
+        AT = "@GeorgeRussell63"
+    else:
+        AT = randomat()
+# Check for repetition
+    with open("volume/" + "oldtweets.txt") as file:
+        OLDTWEETS = file.read().splitlines()
+
+    OLDTWEETFILE = open('volume/' + 'oldtweets.txt', 'a')
+    TWEET = COMMENT[0] + " " + AT + " #F1"
+    if COMMENT[1] not in OLDTWEETS and len(TWEET) < 280:
+        api.update_status(TWEET)
+        print("Tweeted: " + TWEET)
+        OLDTWEETFILE.write(COMMENT[1] + "\n")
+        OLDTWEETFILE.close()
+    elif len(TWEET) > 280:
+        print("Unable to tweet: " + TWEET)
+        print("Tweet too long")
+    elif COMMENT[1] in OLDTWEETS:
+        print("Did not tweet: " + TWEET)
+        print("Duplicate: " + COMMENT[1])
+    else:
+        print("Unknown error.")
+
+
+try:
+    while True:
+        sendit()
+        sleep(3600)
+except KeyboardInterrupt:
+    print("Interrupted!")
