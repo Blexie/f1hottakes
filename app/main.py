82222899286a6550c311edd14b2e2715f186dddd
Blexie <fraser.blexie@gmail.com>
2021-12-11 15:25:48 +0000

    Improved searching for spicy comments.

--- app/main.py
+++ app/main.py
@@ -26,32 +26,46 @@ reddit = praw.Reddit(
 
 # Generate tweet body (controversial threads)
 def generatetweet():
+    BREAKFLAG = None
     for submission in reddit.subreddit("formula1").controversial("day", limit=1):
         submission.comment_sort = "controversial"
-        if not submission.stickied or "Race Discussion" in submission.title:
-          for comment in submission.comments:
-            if comment.author == "automoderator" or comment.stickied or comment.body == "[removed]" or len(comment.body) > 280:
-                continue
-            else:
-                COMMENT = comment.body
-                ID = comment.id
-                break
+        if BREAKFLAG is None:
+            if "Race Discussion" in submission.title or not submission.stickied:
+                for comment in submission.comments:
+                    if comment.author == "automoderator" or comment.stickied or comment.body == "[removed]" or comment.body == "[deleted]" or len(comment.body) > 280:
+                        print("Skipping comment: " + comment.permalink)
+                        continue
+                    else:
+                        COMMENT = comment.body
+                        ID = comment.id
+                        BREAKFLAG = True
+                        print("Comment selected: " + comment.permalink)
+                        break
+        else:
+            break
     return COMMENT, ID
 
 
 # Generate tweet body (hot threads)
 def generatetweethot():
+    BREAKFLAG = None
     for submission in reddit.subreddit("formula1").hot(limit=10):
         submission.comment_sort = "controversial"
-        if not submission.stickied or "Race Discussion" in submission.title:
-          for comment in submission.comments:
-            if comment.author == "automoderator" or comment.stickied or comment.body == "[removed]" or len(comment.body) > 280:
-                continue
-            else:
-                COMMENT = comment.body
-                ID = comment.id
-                break
+        if BREAKFLAG is None:
+            if "Race Discussion" in submission.title or "Qualifying Discussion" in submission.title or not submission.stickied:
+                for comment in submission.comments:
+                    if comment.author == "automoderator" or comment.stickied or comment.body == "[removed]" or comment.body == "[deleted]" or len(comment.body) > 280:
+                        print("Skipping comment: " + comment.permalink)
+                        continue
+                    else:
+                        COMMENT = comment.body
+                        ID = comment.id
+                        BREAKFLAG = True
+                        print("Comment selected: " + comment.permalink)
+                        break
+        else: break
     return COMMENT, ID
+
 
 
 # Random @ Mention
@@ -69,16 +83,40 @@ def sendit():
     hotornot = [generatetweet, generatetweethot]
     COMMENT = choice(hotornot)()
 
-    RBRMATCHES = ["rbr", "RBR", "Max", "Red Bull", "Jos"]
-    WILLIAMSMATCHES = ["williams"]
-    RUSSELLMATCHES = ["Russell"]
+    RBRMATCHES = ["rbr" "max", "red bull", "jos", "verstappen", "perez"]
+    WILLIAMSMATCHES = ["williams", "latifi"]
+    RUSSELLMATCHES = ["russell"]
+    MERCEDESMATCHES = ["lewis", "mercedes", "merc", "hamilton", "bottas"]
+    ALPINEMATCHES = ["fernando", "alonso", "alpine", "ocon"]
+    ASTONMATCHES = ["stroll", "seb", "vettel", "aston"]
+    FERRARIMATCHES = ["charles", "leclerc", "carlos", "sainz", "ferrari", "sbinalla"]
+    MCLARENMATCHES = ["lando", "norris", "danny", "danny ric", "mclaren"]
+    ALFAMATCHES = ["alfa", "kimi", "gio"]
+    HAASMATCHES = ["mick", "nikita", "mazepin", "haas", "steiner"]
+    ALPHATMATCHES = ["yuki", "tsunoda", "pierre", "gasly", "alpha tauri"]
 
     if any(x in COMMENT[0] for x in RBRMATCHES):
         AT = "@redbullracing"
-    elif any(x in COMMENT[0] for x in WILLIAMSMATCHES):
+    elif any(x in COMMENT[0].lower() for x in WILLIAMSMATCHES):
         AT = "@WilliamsRacing"
-    elif any(x in COMMENT[0] for x in RUSSELLMATCHES):
+    elif any(x in COMMENT[0].lower() for x in RUSSELLMATCHES):
         AT = "@GeorgeRussell63"
+    elif any(x in COMMENT[0].lower() for x in MERCEDESMATCHES):
+        AT = "@MercedesAMGF1"
+    elif any(x in COMMENT[0].lower() for x in ALPINEMATCHES):
+        AT = "@AlpineF1Team"
+    elif any(x in COMMENT[0].lower() for x in ASTONMATCHES):
+        AT = "@AstonMartinF1"
+    elif any(x in COMMENT[0].lower() for x in FERRARIMATCHES):
+        AT = "@ScuderiaFerrari"
+    elif any(x in COMMENT[0].lower() for x in MCLARENMATCHES):
+        AT = "@McLarenF1"
+    elif any(x in COMMENT[0].lower() for x in ALFAMATCHES):
+        AT = "@alfaromeoracing"
+    elif any(x in COMMENT[0].lower() for x in HAASMATCHES):
+        AT = "@HaasF1Team"
+    elif any(x in COMMENT[0].lower() for x in ALPHATMATCHES):
+        AT = "@AlphaTauriF1"
     else:
         AT = randomat()
 # Check for repetition
